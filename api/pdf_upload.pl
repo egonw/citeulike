bd9f60299e43ef23c8e3ad44e6699753d04f9f51
fgallagh <fgallagh@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2009-10-17 15:50:19 +0000

    PDF upload script - tidy up code
    git-svn-id: http://svn.citeulike.org/svn@472 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- api/pdf_upload.pl
+++ api/pdf_upload.pl
@@ -5,45 +5,73 @@ use HTTP::Request::Common;
 use JSON;
 use File::Spec;
 
+
+
+#-----------------------------------------------------------------
+# data on STDIN, suck in in one gulp
 my $old = $/;
 undef $/;
-# data on STDIN
 my $DATA = <>;
 $/=$old;
-
 print $DATA;
 
+# Parse the JSON string, using a "relaxed" parser (extra commas and "#" OK)
 my $json = JSON->new;
-
 $json = $json->relaxed(1);
 
 my $DATA = $json->decode($DATA);
 
+#-----------------------------------------------------------------
+# get a "browser", and use cookies
 my $ua = LWP::UserAgent->new;
-
 $ua->cookie_jar( {} );
 
+#-----------------------------------------------------------------
+# Get the base URL - pretty much just for testing
 my $base = $DATA->{baseurl} || "http://www.citeulike.org";
 my $basepath = $DATA->{basepath} || ".";
 
 my $post_username = $DATA->{post_username} || $DATA->{username};
 
-my $res = $ua->request(POST $base."/login.json", [
+my $URL = {
+	login => $base."/login.json",
+	upload => $base."/personal_pdf_upload.json"
+};
+
+
+#-----------------------------------------------------------------
+# LOGIN
+my $res = $ua->request(POST $URL->{login}, [
 	username => $DATA->{username},
 	password => $DATA->{password}
 ]);
 
 status($res);
 
+# We'll bail if not logged in.
+if (!$res->is_success) {
+	exit 1;
+} else {
+	$res = $json->decode($res->content);
+	if ($res->{status} ne "ok") {
+		exit 1;
+	}
+}
+
+#-----------------------------------------------------------------
+# Loop over all the "files" entries
 my @files = @{$DATA->{files}};
 
 foreach my $f (@files) {
 	my $user = $f->{username} || $post_username;
+
+	# path to PDF, using "basepath" in header as default
+	# i.e., path can be either absolute or relative to basepath
 	my $abs_path = File::Spec->rel2abs( $f->{path}, $basepath );
 
 	print "Uploading: ".$abs_path." to ".$user."/".$f->{article_id}."\n";
 
-	my $res = $ua->request(POST $base."/personal_pdf_upload.json",
+	my $res = $ua->request(POST $URL->{upload},
 		Content_Type => 'multipart/form-data',
 		Content => [
 				username => $user,
@@ -57,7 +85,7 @@ foreach my $f (@files) {
 	status($res);
 }
 
-
+#<status>#######################################################################
 sub status {
 	my ($res) = @_;
 	if ($res->is_success) {
