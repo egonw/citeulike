049d4cb785da1f88e669c829e25d4eb3e2230b9f
fgallagh <fgallagh@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2012-01-27 17:29:40 +0000

    Update liebert for new URL and convert to use atypon
    git-svn-id: http://svn.citeulike.org/svn@708 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- plugins/python/liebert.py
+++ plugins/python/liebert.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2006 Kristinn B. Gylfason <citeulike@askur.org>
-# Copyright (c) 2007 Dr. Nathan Edwards
-#
+# Copyright (c) 2011 Fergus Gallagher <fergus.gallagher@citeulike.org>
 # All rights reserved.
-#
-# This code is derived from software contributed to CiteULike.org
-# by
-#    Diwaker Gupta
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -39,97 +33,9 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 
-
-import re, sys, urllib, urllib2, cookielib
+import atypon
 import socket
 
 socket.setdefaulttimeout(15)
 
-
-CITATION_SERVER_ROOT = 'http://www.liebertonline.com/action/downloadCitation'
-DOI_URL_SEP ='%2F'
-DOI_SEP ='/'
-
-# regexp for document object identifiers (DOI)
-DOI_REGEXP = r"""/		# begin at slash
-		(10\.\S*?)/	# valid DOI prefixes start with '10.'
-		([^/?]+)		# DOI suffixes can be anything
-		\s*"""		# terminate at end of line (strip witesp.)
-DOI_REGEXP_FLAGS = re.IGNORECASE | re.VERBOSE
-
-# error messages
-ERR_STR_PREFIX = 'status\terr\t'
-ERR_STR_FETCH = 'Unable to fetch the bibliographic data: '
-ERR_STR_TRY_AGAIN = 'The server may be down.  Please try later.'
-ERR_STR_NO_DOI = 'No document object identifier found in the URL: '
-ERR_STR_REPORT = 'Please report the error to plugins@citeulike.org.'
-
-# read url from std input
-url = sys.stdin.readline()
-# get rid of the newline at the end
-url = url.strip()
-
-# 'unparse' url to remove %HH escapes which confuse the DOI parser below
-url = urllib2.unquote(url)
-
-# parse the DOI from the url and exit gracefully if not found
-doi_match  = re.search(DOI_REGEXP, url, DOI_REGEXP_FLAGS)
-if not doi_match:
-	print ERR_STR_PREFIX + ERR_STR_NO_DOI + url + '.  ' + ERR_STR_REPORT
-	raise
-
-doi_prefix = doi_match.group(1)
-doi_suffix = doi_match.group(2)
-url_doi = doi_prefix + DOI_URL_SEP + doi_suffix
-doi = doi_prefix + DOI_SEP + doi_suffix
-
-# fetch the BibTeX entry for the DOI and exit gracefully in case of trouble
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-post_data = urllib.urlencode( { "doi" : doi,
-				"include" : "abs",
-				"format" : "bibtex",
-				"direct" : "on",
-				"submit" : "Download references"} )
-try:
-	# Cookie me...
-	opener.open("http://www.liebertonline.com/action/showCitFormats?doi=%s" % url_doi)
-	# ... and fetch the bibtex record
-	f = opener.open(CITATION_SERVER_ROOT, post_data)
-except:
-	print ERR_STR_PREFIX + ERR_STR_FETCH + CITATION_SERVER_ROOT + '.  ' + ERR_STR_TRY_AGAIN
-	raise
-
-bibtex_entry = f.read().strip()
-
-# get rid of the session id in the url
-url_pat = re.compile(r';jsessionid.*$',re.MULTILINE)
-bibtex_entry = re.sub(url_pat,'',bibtex_entry)
-# Give the article a proper ID
-bibtex_entry = re.sub( r'(@.*?){,', r'\1{liebert_import,', bibtex_entry)
-
-######
-###### Fix for bibtex(?) not handling one of my co-authors correctly: Chau-Wen Tseng.
-###### Wrap hyphenated non-surnames in { and }.
-######
-m = re.search(r'author\s*=\s*{\s*(.*)\s*}\s*,$',bibtex_entry,re.M)
-if m:
-	auths = m.group(1).split(' and ')
-	for a in auths:
-		l,rest = map(lambda s: s.strip(),a.split(','))
-		fs = map(lambda s: s.strip(),rest.split())
-		for f in fs:
-			if re.search(r'-',f):
-				bibtex_entry = re.sub( r'%s'%re.escape(f), r'{%s}'%f, bibtex_entry)
-
-# print the results
-print "begin_tsv"
-print "linkout\tMALIE\t\t%s\t\t" % (doi)
-print "linkout\tDOI\t\t%s\t\t" % (doi)
-print "type\tJOUR"
-print "doi\t" + doi
-print "end_tsv"
-print "begin_bibtex"
-print bibtex_entry
-print "end_bibtex"
-print "status\tok"
+atypon.process("online.liebertpub.com", "MALIE")
