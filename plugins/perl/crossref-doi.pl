e86def35535d66a801ae90c094cd7906b074a9f7
fgallagh <fgallagh@eb686afc-39fa-0310-a79f-9b3c69cefa1c>
2012-07-09 09:14:32 +0000

    Remove old crud
    git-svn-id: http://svn.citeulike.org/svn@765 eb686afc-39fa-0310-a79f-9b3c69cefa1c

--- plugins/perl/crossref-doi.pl
+++ plugins/perl/crossref-doi.pl
@@ -1,12 +1,4 @@
 #!/usr/bin/perl -w
-
-# use module
-use XML::Simple;
-use Data::Dumper;
-use warnings;
-use LWP 5.64;
-use Encode;
-
 #
 # Copyright (c) 2008 Fergus Gallagher, CiteULike.org
 # All rights reserved.
@@ -41,80 +33,25 @@ use Encode;
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+use warnings;
+use Encode;
 
 binmode STDOUT, ":utf8";
-
-my $browser = LWP::UserAgent->new;
-$browser->cookie_jar({}); #in case we need cookies
-$browser->timeout(10); # secs
 
 $in_url = <>;
 chomp($in_url);
 
 if (! $in_url =~ m{^http://dx\.doi\.org/}i ) {
-	print "status\terr\tUnable to process that URL\n";
+	print "status\terr\t$in_url is not a DOI\n";
 	exit;
 }
 
-$in_url =~ m{^http://dx\.doi\.org/(.*)}i;
-
-# print "$in_url\n";
-
-$doi = $1;
-
-# pid=username:password (private!)
-my $pid=`cat $ENV{HOME}/.crossref-key`;
-
-$pid =~ s/\s+//g;
-
-$url="http://www.crossref.org/openurl/?id=doi:$doi&noredirect=true&pid=$pid&format=unixref";
-
-$response = $browser->get("$url") or (print "status\terr\t Could not retrieve information from the specified page. Try posting the article from the abstract page.\n" and exit);
-$body = $response->content;
-$body = decode("utf-8", $body); # why is this needed?
-
-#
-# Bizarrely, there's no XML decl ("<?xml..") so the encoding's assumed somewhere
-# (not here).
-#
-$xml = new XML::Simple;
-
-#
-# Emergency bodge to fix completely toileted XML from crossref
-#
-#print "$body\n";
-#$body =~ m{(<doi_record>.*</doi_record>)}s or do {
-#	print "status\terr\t  Could not retrieve the information for that DOI. Invalid XML\n";
-#	exit;
-#};
-#$body = $1;
-
-# parse XML (not really necessary as crossref.tcl has to do this anyway)
-
-$data = $xml->XMLin($body, ForceArray => [qw/title titles person_name identifier issn/]);
-
-$base = $data->{"doi_record"}->{"crossref"};
-if (!$base) {
-	$base = $data->{"crossref"};
-}
-
-
-if (!$base) {
-	print "status\terr\t  That DOI does not appear to be a known type. $doi\n";
-	exit;
-}
-
-$error = $data->{"doi_record"}->{"crossref"}->{"error"};
-if ($error) {
-	print "status\terr\t  Could not retrieve the information for that DOI. Crossref returned: $error\n";
-	exit;
-}
+($doi) = $in_url =~ m{^http://dx\.doi\.org/(.*)}i;
 
 print "begin_tsv\n";
-# print "type\tJOUR\n";
+print "linkout\tDOI\t\t$doi\t\t\n";
+# defer to driver.tcl
+print "use_crossref\t1\n";
 print "url\t$in_url\n";
 print "end_tsv\n";
-print "begin_crossref\n";
-print "$body\n";
-print "end_crossref\n";
 print "status\tok\n";
